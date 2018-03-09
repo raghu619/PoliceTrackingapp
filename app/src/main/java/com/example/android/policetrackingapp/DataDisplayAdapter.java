e2ca340ecb60fc69e997b6ea3f93a2fc3fdd8b8b
raghu619 <rgkoduri@gmail.com>
2018-03-10 00:04:50 +0530

    This my fourth commit

---
+++ app/src/main/java/com/example/android/policetrackingapp/DataDisplayAdapter.java
@@ -0,0 +1,97 @@
+package com.example.android.policetrackingapp;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.ArrayList;
+
+/**
+ * Created by raghvendra on 9/3/18.
+ */
+
+public class DataDisplayAdapter  extends RecyclerView.Adapter<DataDisplayAdapter.DataDisplayAdapterViewHolder> {
+
+    private final Context mContext;
+    private ArrayList<Current_Location> arrayList;
+    Current_Location mCurrentdata;
+    private final DataDisplayAdapterOnClickHandler mClickHandler;
+
+
+    public interface DataDisplayAdapterOnClickHandler {
+        void onClick(LatLng latLng);
+    }
+
+    @Override
+    public DataDisplayAdapterViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view= LayoutInflater.from(mContext).inflate(R.layout.list_items,parent,false);
+        view.setFocusable(true);
+
+        return new DataDisplayAdapterViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(DataDisplayAdapterViewHolder holder, int position) {
+         mCurrentdata=arrayList.get(position);
+        holder.Emailview.setText(mCurrentdata.getMail_id());
+        holder.Addressview.setText(mCurrentdata.getMaddress());
+        holder.Timeview.setText(mCurrentdata.getMtime());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if(arrayList==null)
+        return 0;
+        else{
+
+            return arrayList.size();
+        }
+    }
+    public DataDisplayAdapter(Context context,DataDisplayAdapterOnClickHandler mClickHandler){
+        this.mClickHandler=mClickHandler;
+        mContext=context;
+
+    }
+
+    class DataDisplayAdapterViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+
+      final  TextView Emailview;
+      final  TextView Addressview;
+      final  TextView Timeview;
+
+
+        public DataDisplayAdapterViewHolder(View itemView) {
+            super(itemView);
+            Emailview=itemView.findViewById(R.id.emailtext);
+            Addressview=itemView.findViewById(R.id.addresstext);
+            Timeview=itemView.findViewById(R.id.timetext);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+              int adapterPosition = getAdapterPosition();
+            Current_Location location=arrayList.get(adapterPosition);
+            LatLng latLng=new LatLng(Double.parseDouble(location.getLatitude())
+                    ,Double.parseDouble(location.getLongitude()));
+
+            mClickHandler.onClick(latLng);
+
+
+        }
+    }
+
+    public  void setUserdata(ArrayList<Current_Location> list){
+        arrayList=list;
+        notifyDataSetChanged();
+
+
+
+    }
+
+}
---
+++ app/src/main/java/com/example/android/policetrackingapp/NotificationUtils.java
@@ -0,0 +1,67 @@
+package com.example.android.policetrackingapp;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.content.ContextCompat;
+
+/**
+ * Created by raghvendra on 24/2/18.
+ */
+
+public class NotificationUtils {
+
+    private static final int WATER_REMINDER_NOTIFICATION_ID = 1138;
+    /**
+     * This pending intent id is used to uniquely reference the pending intent
+     */
+    private static final int WATER_REMINDER_PENDING_INTENT_ID = 3417;
+    /**
+     * This notification channel id is used to link notifications to this channel
+     */
+    private static final String WATER_REMINDER_NOTIFICATION_CHANNEL_ID = "reminder_notification_channel";
+
+    public static void remindUserBecauseCharging(Context context,String message) {
+        NotificationManager notificationManager = (NotificationManager)
+                context.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(context,WATER_REMINDER_NOTIFICATION_CHANNEL_ID)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_drink_notification)
+                .setLargeIcon(largeIcon(context))
+                .setContentTitle("This just testing ")
+                .setContentText(message)
+                .setStyle(new NotificationCompat.BigTextStyle().bigText("This just testing "))
+                .setDefaults(Notification.DEFAULT_VIBRATE)
+                .setContentIntent(contentIntent(context))
+
+                .setAutoCancel(true);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+                && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
+            notificationBuilder.setPriority(NotificationCompat.PRIORITY_HIGH);
+        }
+        notificationManager.notify(WATER_REMINDER_NOTIFICATION_ID, notificationBuilder.build());
+    }
+    private static PendingIntent contentIntent(Context context) {
+        Intent startActivityIntent = new Intent(context, MainActivity.class);
+        return PendingIntent.getActivity(
+                context,
+                WATER_REMINDER_PENDING_INTENT_ID,
+                startActivityIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+    private static Bitmap largeIcon(Context context) {
+        Resources res = context.getResources();
+        Bitmap largeIcon = BitmapFactory.decodeResource(res, R.drawable.ic_local_drink_black_24px);
+        return largeIcon;
+    }
+}
